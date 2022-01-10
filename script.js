bc7d9545c789895e95dfd9e047585fe55347ee43
aprila13 <aprilacraig1@gmail.com>
2022-01-10 18:38:18 -0500

    completed project

---
+++ script.js
@@ -0,0 +1,26 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+//Toggle Nav
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+});
+
+//Show Modal
+open.addEventListener('click', () => {
+  modal.classList.add('show-modal');
+});
+
+//Close Modal
+close.addEventListener('click', () => {
+  modal.classList.remove('show-modal');
+});
+
+//Hide modal on outside click
+//e.target is wherever you click on the page
+window.addEventListener('click', (e) => {
+  e.target == modal ? modal.classList.remove('show-modal') : false;
+  console.log(modal)
+});
